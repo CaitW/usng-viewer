64e323a44befe961f3ea95af657714230700dcff
CaitW <wolters.caitlin@gmail.com>
2017-12-06 10:16:24 -0600

    Add redux dev tool extension

--- src/js/store.js
+++ src/js/store.js
@@ -1,14 +1,14 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import window from './ducks/window';
-
 
 const theReducer = combineReducers({
     window
 });
+
 const store = createStore(
-    theReducer,
-    // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    theReducer, composeWithDevTools()
 );
+
 export default store;
